09ec26934a7de56fe94385c3765ccd70305ecf7b
iero <iero@users.noreply.github.com>
2018-01-24 21:59:07 +0100

    binance opening date

--- get_historical_klines.py
+++ get_historical_klines.py
@@ -10,7 +10,7 @@ import crypto
 parser = argparse.ArgumentParser()
 parser.add_argument("--params", type=str, help="file.xml", required=True)
 parser.add_argument("--pair", type=str, help="Pair to trade (ALL for all)", required=True)
-parser.add_argument("--year", type=int, help="Year to check", required=True)
+#parser.add_argument("--year", type=int, help="Year to check", required=True)
 option = parser.parse_args()
 
 if __name__ == "__main__":
@@ -26,12 +26,13 @@ if __name__ == "__main__":
 	else :
 		list_pairs.append(option.pair)
 
-	resultat = pd.DataFrame()
-	date = datetime(option.year, 1, 1)
-	date_max = datetime(option.year, 12, 31)
+	# Binance opening : 14/07/2017
+	date = datetime(2017, 7, 14)
+	date_max = datetime(2018, 1, 23)
 
 	loop_timer = 0.5
 	for pair in list_pairs :
+		resultat = pd.DataFrame()
 		while date < date_max :
 			df = crypto.klines.get_historical_klines(client,pair,date)
 			last_date = df.tail(1).index[0]
@@ -41,7 +42,7 @@ if __name__ == "__main__":
 
 			time.sleep(loop_timer)
 
-		file = out_dir+'/klines_'+option.pair+'_'+".csv"
+		file = out_dir+'/klines_'+option.pair+".csv"
 		resultat.to_csv(file)
 
 

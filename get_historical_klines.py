6fc71f142d1552944627c852660bc52d3159f655
iero <iero@users.noreply.github.com>
2018-01-24 21:45:55 +0100

    catch them all

--- get_historical_klines.py
+++ get_historical_klines.py
@@ -9,7 +9,7 @@ import crypto
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--params", type=str, help="file.xml", required=True)
-parser.add_argument("--pair", type=str, help="Pair to trade", required=True)
+parser.add_argument("--pair", type=str, help="Pair to trade (ALL for all)", required=True)
 parser.add_argument("--year", type=int, help="Year to check", required=True)
 option = parser.parse_args()
 
@@ -18,25 +18,31 @@ if __name__ == "__main__":
 	client = crypto.utils.get_binance_client(option.params)
 	out_dir = crypto.utils.get_out_dir(option.params)
 
-	loop_timer = 0.5
+	list_pairs = []
+	if option.pair == ALL :
+		prices = client.get_all_tickers()
+		for price in prices :
+			list_pairs.append(price['symbol'])
+	else :
+		list_pairs.append(option.pair)
 
 	resultat = pd.DataFrame()
-
 	date = datetime(option.year, 1, 1)
 	date_max = datetime(option.year, 12, 31)
 
-	while date < date_max :
-		df = crypto.klines.get_historical_klines(client,option.pair,date)
-		last_date = df.tail(1).index[0]
-		date = last_date
-		print(date)
-		resultat = pd.concat([resultat, df])
+	loop_timer = 0.5
+	for pair in list_pairs :
+		while date < date_max :
+			df = crypto.klines.get_historical_klines(client,pair,date)
+			last_date = df.tail(1).index[0]
+			date = last_date
+			print('[{0}] {1}'.format(pair,date))
+			resultat = pd.concat([resultat, df])
 
-		time.sleep(loop_timer)
+			time.sleep(loop_timer)
 
-
-	file = out_dir+'/klines_'+option.pair+'_'+str(option.year)+".csv"
-	resultat.to_csv(file)
+		file = out_dir+'/klines_'+option.pair+'_'+".csv"
+		resultat.to_csv(file)
 
 
 
